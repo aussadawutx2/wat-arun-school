65b61385cadc3af78588bd825aa570eb0a0085d6
aussadawutx2 <149991607+aussadawutx2@users.noreply.github.com>
2024-03-06 17:11:05 +0700

    edit classroom and bug menu

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -52,11 +52,8 @@ export default function About() {
           </div>
         </div>
         <div className="lg:order-first lg:row-span-2">
-          <h1 className="text-xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-            สำนักเรียน
-            <br />
-            วัดอรุณราชวราราม <br />
-            ราชวรมหาวิหาร
+          <h1 className="text-xl font-bold tracking-tight text-zinc-800 sm:text-2xl dark:text-zinc-100">
+            สำนักเรียน วัดอรุณราชวราราม ราชวรมหาวิหาร
           </h1>
           <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
             <p>
--- src/app/classroom/page.tsx
+++ src/app/classroom/page.tsx
@@ -9,23 +9,23 @@ import logoHelioStream from '@/images/logos/helio-stream.svg'
 import logoOpenShuttle from '@/images/logos/open-shuttle.svg'
 import logoPlanetaria from '@/images/logos/planetaria.svg'
 
-const projects = [
+const classroom = [
   {
-    name: 'Planetaria',
+    name: 'ห้องประโยค 1-2',
     description:
       'Creating technology to empower civilians to explore space on their own terms.',
     link: { href: 'http://planetaria.tech', label: 'planetaria.tech' },
     logo: logoPlanetaria,
   },
   {
-    name: 'Animaginary',
+    name: 'ห้องประโยค 3',
     description:
       'High performance web animation library, hand-written in optimized WASM.',
     link: { href: '#', label: 'github.com' },
     logo: logoAnimaginary,
   },
   {
-    name: 'HelioStream',
+    name: 'ห้องนักธรรมชั้นตรี',
     description:
       'Real-time video streaming library, optimized for interstellar transmission.',
     link: { href: '#', label: 'github.com' },
@@ -59,37 +59,37 @@ function LinkIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
 }
 
 export const metadata: Metadata = {
-  title: 'Projects',
-  description: 'Things I’ve made trying to put my dent in the universe.',
+  title: 'Classroom',
+  description: 'ยินดีต้อนรับเข้าสู่ห้องเรียนของสำนักเรียนของเรา!!!',
 }
 
-export default function Projects() {
+export default function Classroom() {
   return (
     <SimpleLayout
-      title="Things I’ve made trying to put my dent in the universe."
-      intro="I’ve worked on tons of little projects over the years but these are the ones that I’m most proud of. Many of them are open-source, so if you see something that piques your interest, check out the code and contribute if you have ideas for how it can be improved."
+      title="ยินดีต้อนรับเข้าสู่ห้องเรียนของสำนักเรียนของเรา!!!"
+      intro="สำนักเรียนวัดอรุณราชวราราม ได้ทำการเปิดสอนเปรียญธรรมตั้งแต่ประโยค 1-2 จนถึงเปรียญธรรม 9 ประโยค อีกทั้งยังเปิดสอนนักธรรมชั้นตรี-เอก โดยท่านสามารถเลือกห้องเรียนของท่านได้จากห้องเรียนด้านล่างนี้เลย."
     >
       <ul
         role="list"
         className="grid grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:grid-cols-3"
       >
-        {projects.map((project) => (
-          <Card as="li" key={project.name}>
+        {classroom.map((classroom) => (
+          <Card as="li" key={classroom.name}>
             <div className="relative z-10 flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
               <Image
-                src={project.logo}
+                src={classroom.logo}
                 alt=""
                 className="h-8 w-8"
                 unoptimized
               />
             </div>
             <h2 className="mt-6 text-base font-semibold text-zinc-800 dark:text-zinc-100">
-              <Card.Link href={project.link.href}>{project.name}</Card.Link>
+              <Card.Link href={classroom.link.href}>{classroom.name}</Card.Link>
             </h2>
-            <Card.Description>{project.description}</Card.Description>
-            <p className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-teal-500 dark:text-zinc-200">
+            <Card.Description>{classroom.description}</Card.Description>
+            <p className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-yellow-500 dark:text-zinc-200">
               <LinkIcon className="h-6 w-6 flex-none" />
-              <span className="ml-2">{project.link.label}</span>
+              <span className="ml-2">{classroom.link.label}</span>
             </p>
           </Card>
         ))}
